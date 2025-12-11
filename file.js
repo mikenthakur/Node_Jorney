ec8c5019f3fa0e48146d55e91b8c195c04388939
Miken Thakur <mikenthakur7@gmail.com>
2025-12-11 07:27:33 +0545

    file handling

--- file.js
+++ file.js
@@ -9,4 +9,18 @@ const fs = require("fs")
 //     (err) => {}
 // )
 
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
 
+// fs.readFile("./contacts.txt", "utf8",
+//     (err, result) => {
+//         if(err) {
+//             console.log("Error", err);
+//         }else{
+//             console.log(result)
+//         }
+//     }
+// )
+
+
+fs.appendFileSync("./test.txt", `${Date.now()} Hey There\n`)

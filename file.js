3475c60956ad21875aa9987e84367829570fa25b
Miken Thakur <mikenthakur7@gmail.com>
2025-12-12 22:22:05 +0545

    updated

--- file.js
+++ file.js
@@ -24,3 +24,12 @@ const fs = require("fs")
 
 
 fs.appendFileSync("./test.txt", `${Date.now()} Hey There\n`)
+
+// fs.cpSync("./test.txt", "./copy.txt");
+
+// fs.unlinkSync("./copy.txt")
+
+console.log(fs.statSync("./test.txt"))
+
+//fs.mkdirSync('my-docs/ ', {})
+
